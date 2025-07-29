a5e492392a5c5ff2887bb0f2f9061b26d1eac131
Sahil Kumar Sinha <sahilkumarsinha@Sahils-MacBook-Pro.local>
2025-07-29 16:36:27 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Router as WouterRouter, Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -8,13 +8,25 @@ import Dashboard from "@/pages/dashboard";
 import OperationalDashboard from "@/pages/operational-dashboard";
 import NotFound from "@/pages/not-found";
 
+// function Router() {
+//   return (
+//     <Switch>
+//       <Route path="/Amul-dashboard/" component={Dashboard} />
+//       {/* <Route path="/operational-dashboard" component={OperationalDashboard} /> */}
+//       <Route component={NotFound} />
+//     </Switch>
+//   );
+// }
+
 function Router() {
   return (
-    <Switch>
-      <Route path="/Amul-dashboard/" component={Dashboard} />
-      {/* <Route path="/operational-dashboard" component={OperationalDashboard} /> */}
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter base="/Amul-dashboard">
+      <Switch>
+        <Route path="/" component={Dashboard} />
+        {/* <Route path="/operational-dashboard" component={OperationalDashboard} /> */}
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
